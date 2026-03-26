050376b2bd97f89e4709b7e12b44c5bb350e882f
Will Kelly <bkelldog59@gmail.com>
2026-03-26 09:02:56 -0400

    Add mobile styling.

--- source/scripts/home.js
+++ source/scripts/home.js
@@ -13,3 +13,19 @@ function alumniBtn() {
   alumSection.classList.remove("hidden");
   btn.classList.add("hidden");
 }
+
+function undergradBtnMobile() {
+  const UGsection = document.getElementById("undergrads-section");
+  const alumSection = document.getElementById("alumni-section");
+
+  UGsection.classList.toggle("hidden");
+  alumSection.classList.add("hidden");
+}
+
+function alumniBtnMobile() {
+  const alumSection = document.getElementById("alumni-section");
+  const UGsection = document.getElementById("undergrads-section");
+
+  alumSection.classList.toggle("hidden");
+  UGsection.classList.add("hidden");
+}
